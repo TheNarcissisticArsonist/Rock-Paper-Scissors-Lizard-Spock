c8a714374c8694a34bbc7f08e2206e80798814eb
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-13 15:34:20 -0400

    Added comments.

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,28 @@
+/*
+ * Rock, Paper, Scissors, Lizard, Spock
+ *
+ * Rock beats scissors and lizard.
+ * Paper beats rock and Spock.
+ * Scissors beats paper and lizard.
+ * Lizard beats paper and Spock
+ * Spock beats rock and scissors
+ *
+ * Difficulty:
+ * Easy			The computer guesses randomly
+ * Hard			The computer removes one of two losing options based on yours
+ * Impossible	The computer has a 100% chance of winning.
+ *
+ * It's impossible, right?
+ */
+
+
 #include <iostream>
 #include <stdio.h>
 
 using namespace std;
 
 int main() {
+	//Ask the user to specify difficulty level
 	string difficulty = "";
 	cout << "What difficulty level would you like?" << endl;
 	cout << "Easy, Hard, or Impossible?" << endl;
@@ -13,4 +32,5 @@ int main() {
 		cin >> difficulty;
 	}
 	cout << difficulty << endl;
+
 }

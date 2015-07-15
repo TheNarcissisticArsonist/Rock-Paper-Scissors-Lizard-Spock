5ad95c54081b1f70cde874f34fa9b1499c3e16e5
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-15 14:36:42 -0400

    Added difficulty from user.

--- Main.cpp
+++ Main.cpp
@@ -8,9 +8,9 @@
  * Spock beats rock and scissors
  *
  * Difficulty:
- * Easy			The computer guesses randomly
- * Hard			The computer gets two tries
- * Impossible	The computer has a 100% chance of winning.
+ * 1	Easy			The computer guesses randomly
+ * 2	Hard			The computer gets two tries
+ * 3	Impossible	The computer has a 100% chance of winning.
  *
  * It's impossible, right?gj
  */
@@ -22,6 +22,40 @@
 
 using namespace std;
 
-int main() {
+void toLowercase(string &text) {
+	for(int i=0, len = text.length(); i<len; ++i) {
+		if(text[i] <= 90) {
+			text[i] = text[i]+32;
+		}
+	}
+}
 
+int getDifficulty() {
+	cout << "What difficulty would you like to play?" << endl;
+	cout << "Easy, hard, or impossible?" << endl;
+	string myChoice;
+	cin >> myChoice;
+	toLowercase(myChoice);
+	if(myChoice == "easy") {
+		return 1;
+	}
+	else if(myChoice == "hard") {
+		return 2;
+	}
+	else if(myChoice == "impossible") {
+		return 3;
+	}
+	else {
+		return 999;
+	}
+}
+
+int main() {
+	int difficulty = getDifficulty();
+	cout << "You chose to play on difficulty level " << difficulty << "." << endl;
+	cout << endl;
+	//int myChoice = getUserChoice();
+	cout << endl;
+	//int compChoice = getCompChoice(myChoice, difficulty);
+	cout << endl;
 }
